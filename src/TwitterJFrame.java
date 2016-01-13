b9f176a117832b77171e4286c570c33911185c99
341167922 <341167922@109R030573ZHQ22.EDU.YRDSB.NET>
2016-01-13 11:23:31 -0500

    hyghgh

--- src/TwitterJFrame.java
+++ src/TwitterJFrame.java
@@ -4,8 +4,28 @@
  * and open the template in the editor.
  */
 
+import java.util.List;
 import twitter4j.*;
-import javax.swing.JOptionPane;
+import twitter4j.auth.AccessToken;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  *
@@ -13,8 +33,6 @@ import javax.swing.JOptionPane;
  */
 public class TwitterJFrame extends javax.swing.JFrame {
 
-    
-    
     /**
      * Creates new form TwitterJFrame
      */
@@ -32,17 +50,11 @@ public class TwitterJFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jLabel2 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
         jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel2.setText("My Last Tweet");
-
-        jTextField2.setText("jTextField2");
-
-        jButton2.setText("Login");
+        jButton2.setText("Update Tweets");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
@@ -54,42 +66,104 @@ public class TwitterJFrame extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel2)
-                .addGap(18, 18, 18)
-                .addComponent(jTextField2)
-                .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addGap(77, 77, 77)
+                .addGap(94, 94, 94)
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 194, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(129, Short.MAX_VALUE))
+                .addContainerGap(112, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(119, 119, 119)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
                 .addComponent(jButton2)
-                .addContainerGap(120, Short.MAX_VALUE))
+                .addContainerGap(158, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        try{
-            Twitter twitter = new Twitter("NHSDisplay","twitterapi");
-            twitter.verifyCredentials();
-            JOptionPane.showMessageDialog(null,"You're logged in!");
-            java.util.List<Status> statusList = twitter.getUserTimeline();
-            String s = String.valueOf(statusList.get(0).getText());
-            jTextField2.setText(s);
-        }catch(TwitterException e){
-            JOptionPane.showMessageDialog(null,"Login failed");
+  //Your Twitter App's Consumer Key
+         String consumerKey = "iuQCb5R7W28H46DAMzUHM2slo";
+
+         //Your Twitter App's Consumer Secret
+         String consumerSecret = "pZxzt1uHOoKByI1Gr6ub0lUME3K4xLdxLzKvzfZsfHYesd5bJ5";
+
+         //Your Twitter Access Token
+         String accessToken = "4736054183-0r6cUOMERw9Y3lSqZAefJxFSOJ9LHWaa90WlP84";
+
+         //Your Twitter Access Token Secret
+         String accessTokenSecret = "luF2CzgnRLmpNhK5snVJn6FvsQcwMeaOuQXTO5la8KKz1";
+
+         //Instantiate a re-usable and thread-safe factory
+         TwitterFactory twitterFactory = new TwitterFactory();
+
+         //Instantiate a new Twitter instance
+         Twitter twitter = twitterFactory.getInstance();
+
+         //setup OAuth Consumer Credentials
+         twitter.setOAuthConsumer(consumerKey, consumerSecret);
+
+         //setup OAuth Access Token
+         twitter.setOAuthAccessToken(new AccessToken(accessToken, accessTokenSecret));
+
+         
+        try {
+            List<Status> statuses = twitter.getHomeTimeline();
+            System.out.println("Showing home timeline:");
+            for (Status status:statuses){
+                System.out.println(status.getUser().getName() + ":" + status.getText());
+            }
+            
+            //Instantiate and initialize a new twitter status update
+            //StatusUpdate statusUpdate = new StatusUpdate("NHS Display's First Tweet!");
+            
+            //+ " - http://h1b-work-visa-usa.blogspot.com/2013/07/h-1b-transfer-jobs-java-developer_19.html");
+            //attach any media, if you want to
+            /*
+            statusUpdate.setMedia(
+            //title of media
+            "http://h1b-work-visa-usa.blogspot.com", new URL("http://lh6.ggpht.com/-NiYLR6SkOmc/Uen_M8CpB7I/AAAAAAAAEQ8/tO7fufmK0Zg/h-1b%252520transfer%252520jobs%25255B4%25255D.png?imgmax=800").openStream());
+            
+            //tweet or update status
+            Status status = twitter.updateStatus(statusUpdate);
+            
+            //response from twitter server
+            System.out.println("status.toString() = " + status.toString());
+            System.out.println("status.getInReplyToScreenName() = " + status.getInReplyToScreenName());
+            System.out.println("status.getSource() = " + status.getSource());
+            System.out.println("status.getText() = " + status.getText());
+            System.out.println("status.getContributors() = " + Arrays.toString(status.getContributors()));
+            System.out.println("status.getCreatedAt() = " + status.getCreatedAt());
+            System.out.println("status.getCurrentUserRetweetId() = " + status.getCurrentUserRetweetId());
+            System.out.println("status.getGeoLocation() = " + status.getGeoLocation());
+            System.out.println("status.getId() = " + status.getId());
+            System.out.println("status.getInReplyToStatusId() = " + status.getInReplyToStatusId());
+            System.out.println("status.getInReplyToUserId() = " + status.getInReplyToUserId());
+            System.out.println("status.getPlace() = " + status.getPlace());
+            System.out.println("status.getRetweetCount() = " + status.getRetweetCount());
+            System.out.println("status.getRetweetedStatus() = " + status.getRetweetedStatus());
+            System.out.println("status.getUser() = " + status.getUser());
+            System.out.println("status.getAccessLevel() = " + status.getAccessLevel());
+            System.out.println("status.getHashtagEntities() = " + Arrays.toString(status.getHashtagEntities()));
+            System.out.println("status.getMediaEntities() = " + Arrays.toString(status.getMediaEntities()));
+            if (status.getRateLimitStatus() != null) {
+            System.out.println("status.getRateLimitStatus().getLimit() = " + status.getRateLimitStatus().getLimit());
+            System.out.println("status.getRateLimitStatus().getRemaining() = " + status.getRateLimitStatus().getRemaining());
+            System.out.println("status.getRateLimitStatus().getResetTimeInSeconds() = " + status.getRateLimitStatus().getResetTimeInSeconds());
+            System.out.println("status.getRateLimitStatus().getSecondsUntilReset() = " + status.getRateLimitStatus().getSecondsUntilReset());
+            //  System.out.println("status.getRateLimitStatus().getRemainingHits() = " + status.getRateLimitStatus().getRemainingHits());
+            }
+            System.out.println("status.getURLEntities() = " + Arrays.toString(status.getURLEntities()));
+            System.out.println("status.getUserMentionEntities() = " + Arrays.toString(status.getUserMentionEntities()));
+            
+            }catch(TwitterException e){
+            
+            }
+            */
+        } catch (TwitterException ex) {
+            java.util.logging.Logger.getLogger(TwitterJFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
+
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
@@ -127,10 +201,9 @@ public class TwitterJFrame extends javax.swing.JFrame {
         });
     }
 
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
